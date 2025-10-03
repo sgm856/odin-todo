5f35dae80c79ed95b914c1451ab8466cf32c1232
sgm856 <179048454+sgm856@users.noreply.github.com>
2025-10-02 18:51:00 -0600

    skeelton

---
+++ webpack.common.js
@@ -0,0 +1,26 @@
+// webpack.config.js
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+        }),
+    ],
+};
+
