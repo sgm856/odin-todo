8398ef38b71c968fe7f6069142ca30fd2321fde6
sgm856 <179048454+sgm856@users.noreply.github.com>
2025-10-07 06:46:38 -0600

    setup done

--- webpack.common.js
+++ webpack.common.js
@@ -1,21 +1,13 @@
-// webpack.config.js
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+    mode: "development",
     entry: "./src/index.js",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
         clean: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource",
-            },
-        ],
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -23,4 +15,3 @@ module.exports = {
         }),
     ],
 };
-
